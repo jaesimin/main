49c79687d94606182cff4fead6a1646eed35f23f
JasmineYeo_A0185321L <jas.mint98@gmail.com>
2019-10-14 10:19:35 +0800

    basic delete command (not finished)

---
+++ src/main/java/seedu/address/logic/commands/DeleteCheatsheetCommand.java
@@ -0,0 +1,50 @@
+package seedu.address.logic;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.CommandResult;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.CheatsheetModel;
+import seedu.address.model.cheatsheet.Cheatsheet;
+
+import java.util.List;
+
+import static java.util.Objects.requireNonNull;
+
+public class DeleteCheatsheetCommand {
+    public static final String COMMAND_WORD = "delete";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Deletes the cheatsheet identified by the index number used in the displayed cheatsheet list.\n"
+            + "Parameters: INDEX (must be a positive integer)\n"
+            + "Example: " + COMMAND_WORD + " 1";
+
+    public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted Cheatsheet: %1$s";
+
+    private final Index targetIndex;
+
+    public DeleteCheatsheetCommand(Index targetIndex) {
+        this.targetIndex = targetIndex;
+    }
+
+    @Override
+    public CommandResult execute(CheatsheetModel model) throws CommandException {
+        requireNonNull(model);
+        List<Cheatsheet> lastShownList = model.getFilteredCheatsheetList();
+
+        if (targetIndex.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+
+        Cheatsheet cheatsheetToDelete = lastShownList.get(targetIndex.getZeroBased());
+        model.deleteCheatsheet(cheatsheetToDelete);
+        return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, cheatsheetToDelete));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof DeleteCheatsheetCommand // instanceof handles nulls
+                && targetIndex.equals(((DeleteCheatsheetCommand) other).targetIndex)); // state check
+    }
+}
